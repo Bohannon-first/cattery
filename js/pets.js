b6a40df9502ef8cae634e16e545445c3a863c5f1
Bohannon-first <74018115+Bohannon-first@users.noreply.github.com>
2023-02-03 16:31:17 +0000

    Deploying to gh-pages from @ Bohannon-first/cattery@d5d346bcd4d23f3fd1ebe9efd3e86ec3c144196e 🚀

--- js/pets.js
+++ js/pets.js
@@ -1,5 +1,7 @@
 const petsIndicators = document.querySelector('.pets__indicators');
 const petsIndicatorsButtons = petsIndicators.querySelectorAll('button');
+const carouselPets = document.querySelector('#carouselPets');
+const carouselInnerPets = carouselPets.querySelectorAll('.gallery-images');
 
 // Обработчик клика на кнопки слайдера в блоке pets
 const petsIndicatorsClickHandler = (evt) => {
@@ -17,3 +19,25 @@ const petsIndicatorsClickHandler = (evt) => {
 };
 
 petsIndicators.addEventListener('click', petsIndicatorsClickHandler);
+
+// Обработчик клика на кнопки слайдера (лево, право)
+const carouselPetsClickHandler = (evt) => {
+  // Ищем нажатие на кнопки слайдера
+  if (evt.target.closest('.pets__slider-btn-prev') || evt.target.closest('.pets__slider-btn-next')) {
+    setTimeout(() => {
+      carouselInnerPets.forEach((item, index) => {
+        // Находим активный слайд в слайдере
+        if (item.classList.contains('active')) {
+          petsIndicatorsButtons.forEach((button) => {
+            // Удаляем со всех кнопок в слайдере класс active (белый шрифт)
+            button.classList.remove('active');
+          });
+          // Добавляем на активную кнопку класс active с белым шрифтом
+          petsIndicatorsButtons[index].classList.add('active');
+        }
+      });
+    }, 1000);
+  }
+};
+
+carouselPets.addEventListener('click', carouselPetsClickHandler);
